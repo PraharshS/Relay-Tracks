d5f45cf219903dedcda9050c9690c6e5efc70d92
Praharsh Singh <praharsh535@gmail.com>
2020-10-10 13:55:51 +0530

    changes done

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+$(".site-slider").not(".slick-initialized").slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: ".site-slider .slider-btn .prev",
+  nextArrow: ".site-slider .slider-btn .next",
+});
+
+// var btn = document.querySelector(".menu");
+// var nav = document.querySelector(".nav-list");
+
+// btn.addEventListener("click", function () {
+//   console.log("clickd");
+//   nav.classList.toggle("show");
+// });
+
 const words = [
   "Exposure to diverse career paths.",
   "Network of inspiring peers and alumni.",
@@ -9,11 +27,11 @@ const words = [
   "Scope to work on path-breaking projects",
   "Develop skills needed for the real world",
 ];
-let i = 0;
+let K = 0;
 let timer;
 
 function typingEffect() {
-  let word = words[i].split("");
+  let word = words[K].split("");
   var loopTyping = function () {
     if (word.length > 0) {
       document.getElementById("word").innerHTML += word.shift();
@@ -27,16 +45,16 @@ function typingEffect() {
 }
 
 function deletingEffect() {
-  let word = words[i].split("");
+  let word = words[K].split("");
   var loopDeleting = function () {
     if (word.length > 0) {
       word.pop();
       document.getElementById("word").innerHTML = word.join("");
     } else {
-      if (words.length > i + 1) {
-        i++;
+      if (words.length > K + 1) {
+        K++;
       } else {
-        i = 0;
+        K = 0;
       }
       typingEffect();
       return false;
